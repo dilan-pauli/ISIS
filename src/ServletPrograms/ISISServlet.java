29f0611a07a301cb3a42fed70044959e07636700
Dwight <Dwight@unknown20c9d0c3e92d.gateway.2wire.net>
2013-01-17 22:57:50 -0600

    Revert "Revert "Basic WebSocket echo functionality now working""
    This reverts commit 5ead168359fee041792e70a2c97ad7331b519b35.

--- src/ServletPrograms/ISISServlet.java
+++ src/ServletPrograms/ISISServlet.java
@@ -1,22 +1,26 @@
 package ServletPrograms;
 
-import java.io.IOException;
-//import java.io.PrintWriter;
+//import java.io.IOException;
 import java.util.logging.Level;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
 import com.sun.grizzly.websockets.WebSocketEngine;
-//import com.sun.xml.ws.transport.http.servlet.WSServlet;
 
 import WebSocketClasses.ISISServerApplication;
 
-@WebServlet("/ISISServlet")
+/*
+ * loadOnStartup=1 ensures that the servlet gets started as soon as the server gets deployed
+ * And as soon as the server deploys, it will register the ISISServerApplication with the
+ * WebSocketEngine of Grizzly
+ * 
+ * (Hook into Glassfish)
+ */
+@WebServlet(name="ISISServlet", urlPatterns="/ISISServlet", loadOnStartup=1)
 public class ISISServlet extends HttpServlet {
 
 	/**
@@ -27,13 +31,22 @@ public class ISISServlet extends HttpServlet {
 	/**
 	 * The application (WebSocketListener) registered with this servlet
 	 */
-	private ISISServerApplication app = new ISISServerApplication();
+	private ISISServerApplication app;
 
 	/**
 	 * Method to initialize the servlet
+	 * When initializing, register the ISISServerApplication with the WebSocketEngine of Grizzly
 	 */
 	public void init() {
-		// Resister the ISIS application with the WebSocket Engine
+		try {
+			super.init();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+		
+		app = new ISISServerApplication();
+		
+		// Resister the ISIS application with the (Grizzly's) WebSocket Engine
 		WebSocketEngine.getEngine().register(app);
 		
 		// Create log message
@@ -41,16 +54,35 @@ public class ISISServlet extends HttpServlet {
 				Level.INFO, this.getServletName() + "Time: " + new java.util.Date() + ", " + 
 				"initialised servlet and registered ISIS application");
 	}
+	
+	/**
+	 * Method to destroy the servlet
+	 * When destroyed, unregister the ISISServerApplication from the WebSocketEngine of Grizzly
+	 */
+	public void destroy() {
+		super.destroy();
+		
+		// Unregister the application from the (Grizzly's) WebSocket Engine
+		WebSocketEngine.getEngine().unregister(app);
+		
+		// Create log message
+		java.util.logging.Logger.getAnonymousLogger().log(
+				Level.INFO, "Time: " + new java.util.Date() + ", " + 
+						this.getServletName() + " servlet: destroyed");
+	}
+	
+	
+	
 
 	/**
 	 * Method to handle HTTP-GET requests
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	/**public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		// Create log message
 		java.util.logging.Logger.getAnonymousLogger().log(
-				Level.INFO, "Time: " + new java.util.Date() + ", " + "Servlet doGet() event fired");
+				Level.INFO, "Time: " + new java.util.Date() + ", " + "Servlet doGet() event fired");*/
 
 		//HttpSession session = request.getSession(true);
 		//SCRAP request.getContentType().endsWith(arg0)
@@ -74,30 +106,17 @@ public class ISISServlet extends HttpServlet {
 
 		/*out.println("\n" + "SERVER RECEIVED YOUR MESSAGE:" + "\n" +
 				"<br />" + "\n");*/
-	}
+	/**}*/
 
 	/**
 	 * Method to handle HTTP-POST requests
 	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	/**public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Create log message
 		java.util.logging.Logger.getAnonymousLogger().log(
 				Level.INFO, "Time: " + new java.util.Date() + ", " + "Servlet doPost() event fired");
 
 		doGet(request, response);
-	}
-
-	/**
-	 * Method to destroy the servlet
-	 */
-	public void destroy() {
-		// Unregister the application from the WebSocket Engine
-		WebSocketEngine.getEngine().unregister(app);
-		
-		// Create log message
-		java.util.logging.Logger.getAnonymousLogger().log(
-				Level.INFO, "Time: " + new java.util.Date() + ", " + 
-						this.getServletName() + "servlet: destroyed");
-	}
+	}*/
 }
