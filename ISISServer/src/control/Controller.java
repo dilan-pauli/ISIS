8e6d3a95b4955c75813f613727efb5033d7e9e5f
Dwight <Dwight@172.18.16.11>
2013-02-20 00:21:29 -0600

    Added some methods and data structures to Controller. Wrote methods for Xbee state.

--- ISISServer/src/control/Controller.java
+++ ISISServer/src/control/Controller.java
@@ -1,6 +1,6 @@
 package control;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 
 import org.json.simple.JSONObject;
 
@@ -15,22 +15,39 @@ public class Controller {
 	 */
 	private XBeeHandler handler;
 	private WebSocketServer server;
-	
+
 	/**
 	 * Controller Threads
 	 */
+	/* 
+	 * Thread1 monitors the incoming queue of the XbeeHandler, translate items present on this queue 
+	 * from XBee packets to JSON Objects which are understood by browser clients (as specified by the Browser 
+	 * Response API), and then place these translated items on the Outgoing queue of the WebSocket Object 
+	 */
 	private Thread1 xbeeInMonitor;
+	/*
+	 * Thread2 monitors the Incoming queue of the WebSocket Object. It will translate items present 
+	 * on this queue from JSON Objects (as specified by the Browser Request API) and query the XBee state 
+	 * list for the specific information that the client is after.
+	 */
 	private Thread2 webSocketInMonitor;
+	/*
+	 * Timer thread sleeps and periodically wakes up to put XBee packet commands on the XBee 
+	 * Object’s Outgoing queue to check whether controllers are present and updates the XBee state list 
+	 * accordingly
+	 */
 	private Timer timer;
-	
+
 	/**
 	 * Controller Object lists
 	 */
-	// List to store the states of all Xbee devices
-	private LinkedList<XBeeState> xbeeStateList;
+	// List to store the states of all XBee devices
+	//private LinkedList<XBeeState> xbeeStateList;
+	private HashMap<String, XBeeState> xbeeStateList;//K,V => String(ControllerID),XbeeState
 	// List to store the WebSocket clients that are currently connected to the ISIS server
-	private LinkedList<ISISWebSocket> webSocketClientList;
-	
+	//private LinkedList<ISISWebSocket> webSocketClientList;
+	private HashMap<String, ISISWebSocket> webSocketClientList;//K,V => String(WebSocketClientID),ISISWebSocket
+
 	/**
 	 * Constructor
 	 * @param handler
@@ -38,39 +55,102 @@ public class Controller {
 	 */
 	public Controller(XBeeHandler handler, WebSocketServer server)
 	{
+		/*
+		 * Set the handlers for Xbee and WebSocket
+		 */
+		this.handler = handler;
+		this.server = server;
+
+		/*
+		 * Create the Controller lists
+		 */
+		this.xbeeStateList = new HashMap<String, XBeeState>();
+		this.webSocketClientList = new HashMap<String, ISISWebSocket>();
+
 		/* 
-		 * Create Thread1 to monitor the incoming queue of the XbeeHandler, translate items present on this queue 
-		 * from XBee packets to JSON Objects which are understood by browser clients (as specified by the Browser 
-		 * Response API), and then place these translated items on the Outgoing queue of the WebSocket Object 
+		 * Create the Controller threads
 		 */
-		
+		this.xbeeInMonitor = new Thread1();
+		this.webSocketInMonitor = new Thread2();
+		this.timer = new Timer();
+
 		/*
-		 * Create Thread2 to monitor the Incoming queue of the WebSocket Object. It will translate items present 
-		 * on this queue from JSON Objects (as specified by the Browser Request API) and query the XBee state 
-		 * list for the specific information that the client is after.
+		 * Run the Controller threads
 		 */
-		
-		/*
-		 * Create Timer thread that sleeps and periodically wakes up to put XBee packet commands on the XBee 
-		 * Object’s Outgoing queue to check whether controllers are present and updates the XBee state list 
-		 * accordingly
-		 */
+		this.xbeeInMonitor.run();
+		this.webSocketInMonitor.run();
+		this.timer.run();
 	}
-	
+
 	/**
 	 * Controller Object functions
 	 */
+	
 	// Convert XBee Packet to JSON
 	private JSONObject convertXbeePacketToJSON(XBeePacket xbeePkt) {
-		//TODO
+		// TODO MAKE THIS LEGIT	
+		// Figure out what kind of xbee response (packet) it is first
 		return null;
 	}
+	
 	// Convert JSON to XBee Packet
 	private XBeePacket convertJSONToXbeePacket(JSONObject jsonObj) {
-		// TODO
+		// TODO MAKE THIS LEGIT
+		// Figure out what kind of client request (json) it is first
 		return null;
 	}
-	// Accessor functions for Xbee Module list and Xbee Client list
 	
-	// Mutator functions for Xbee Module list and Xbee Client list
+	// Accessor functions for XBee States list
+	
+	/**
+	 * @param id
+	 * @return The state of the Xbee controller with the given ID
+	 */
+	private XBeeState getXBeeStateForController(String id) {
+		return this.xbeeStateList.get(id);
+	}
+	
+	// Accessor functions for WebSocket Client list
+	
+	/**
+	 * @param wsClientId
+	 * @return The WebSocket object for the client with the given ID
+	 */
+	private ISISWebSocket getWebSocketForClient(String wsClientId) {
+		return this.webSocketClientList.get(wsClientId);
+	}
+	
+	// Mutator functions for XBee States list
+	
+	/**
+	 * Add an XBee state the the list of XBee states
+	 * @param state
+	 */
+	private void addXBeeState(XBeeState state) {
+		this.xbeeStateList.put(state.getControllerID(), state);
+	}
+	/**
+	 * Remove the XBee state for the controller with the given id from the list of XBee states
+	 * @param id
+	 */
+	private void removeXBeeState(String id) {
+		this.xbeeStateList.remove(id);
+	}
+	
+	// Mutator functions for WebSocket Client list
+	
+	/**
+	 * Add s WebSocket client to the list of WebSocket clients
+	 * @param client
+	 */
+	private void addWebSocketClient(ISISWebSocket client) {
+		this.webSocketClientList.put(client.toString(), client);
+	}
+	/**
+	 * Remove the WebSocket client with the given id from the list of WebSocket clients
+	 * @param clientId
+	 */
+	private void removeWebSocketClient(String clientId) {
+		this.webSocketClientList.remove(clientId);
+	}
 }
--- ISISServer/src/control/XBeeState.java
+++ ISISServer/src/control/XBeeState.java
@@ -1,17 +1,111 @@
 package control;
 
 public class XBeeState {
+	/**
+	 * Private data fields
+	 */
+	// Controller device ID
 	private String deviceID;
+	
+	// Number of controller buttons
 	private int NUM_IO_PINS = 5;
-	private boolean buttonData[];
-	private boolean isOn;
-	private boolean buttonPinVoltages[];
+	
+	// (UP,DOWN,LEFT,RIGHT,CENTER) => true means pressed
+	private boolean buttonData[] = {false, false, false, false, false};
+	
+	// True if the device with the deviceID is on
+	private boolean deviceIsOn;
+	
+	// (UP,DOWN,LEFT,RIGHT,CENTER) => 0 volts (low) to 3.3 volts (high)
+	private int buttonPinVoltages[] = {0, 0, 0, 0, 0};
 	
 	// TODO: COULD ADD MORE STORED INFO HERE (int powerRemaining, int wirelessStrength, int errorRate)
+
 	
+	/**
+	 * Class constructor
+	 */
 	public XBeeState() {
 		;
 	}
+
 	
-	// TODO: ADD ACCESSORS AND MUTATORS FOR FIELDS
+	// ACCESSOR FUNCTIONS
+	
+	/**
+	 * Obtain the device ID
+	 */
+	public String getControllerID() {
+		return this.deviceID;
+	}
+
+	/**
+	 * Obtain the ON state of the device
+	 */
+	public boolean isOn() {
+		return this.deviceIsOn;
+	}
+
+	/**
+	 * Obtain the state of the button IO pins (UP,DOWN,LEFT,RIGHT,CENTER) => true means pressed
+	 */
+	public boolean[] getButtonIOStates() {
+		return this.buttonData;
+	}
+
+	/**
+	 * Obtain the button pin voltages
+	 */
+	public int[] getButtonPinVoltages() {
+		return this.buttonPinVoltages;
+	}
+
+	
+	// MUTATOR FUNCTIONS
+	
+	/**
+	 * Set the controller ID for the device whose state this is
+	 */
+	public void setControllerID(String id) {
+		this.deviceID = id;
+	}
+
+	/**
+	 * Set the on state of the device (true if the device is on)
+	 */
+	public void setOnState(boolean newOnState) {
+		this.deviceIsOn = newOnState;
+	}
+
+	/**
+	 * Set the state of the button IO pins (pressed or not pressed?)
+	 * @return true if operation succeeded
+	 */
+	public boolean setButtonIOStates(boolean[] newIOStates) {
+		if(newIOStates.length != 5) {
+			return false;
+		}
+		
+		for(int i = 0; i < this.NUM_IO_PINS; i++) {
+			this.buttonData[i] = newIOStates[i];
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Set the button pin voltages
+	 * @return true if operation succeeded
+	 */
+	public boolean setButtonPinVoltages(int[] newPinVoltages) {
+		if(newPinVoltages.length != 5) {
+			return false;
+		}
+		
+		for(int i = 0; i < this.NUM_IO_PINS; i++) {
+			this.buttonPinVoltages[i] = newPinVoltages[i];
+		}
+		
+		return true;
+	}
 }
