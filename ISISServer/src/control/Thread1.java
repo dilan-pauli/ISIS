1861ba693ddda2f0dbb1d4065738bb3b99fc9697
Dilan <dilanp12@gmail.com>
2013-02-26 00:50:27 -0600

    Added some test info to the xbeehandler. Also edited Thread1 to have bare bones. need to talk to dwight about the websocket part and need to plan out the remoteDeviceState List.

--- ISISServer/src/control/Thread1.java
+++ ISISServer/src/control/Thread1.java
@@ -2,38 +2,70 @@ package control;
 
 import java.util.Queue;
 
+import remoteInterface.FromRemoteInterface;
+import remoteInterface.RemoteData;
+
 import datastucts.XBeePacket;
 
 /** 
  * Thread1 monitors the incoming queue of the XbeeHandler, translate items present on this queue 
  * from XBee packets to JSON Objects which are understood by browser clients (as specified by the Browser 
- * Response API), and then places these translated items on the Outgoing queue of the WebSocket Object 
+ * Response API), and then places these translated items on the Outgoing queue of the WebSocket Object.
+ * 
+ *  This tread will look at the remote queues and do two things when a new message is on the queue.
+ *  If that message is a randomly generated button push the packet would be pushed out to all of the 
+ *  client browsers and the remote state data structure will be updated.
  */
 public class Thread1 implements Runnable {
 	
-	private Queue<XBeePacket> toXBeeNetworkQ;
+	private FromRemoteInterface toXBeeNetworkQ;
 	
-	private Queue<XBeePacket> fromXBeeNetworkQ;
+	//TODO This would be the WebSocket Interface to send out to the client
 	
-	private Controller controller;
 	
-	public Thread1() { //(Queue<XBeePacket> toXBeeNetworkQueue, Queue<XBeePacket> fromXBeeNetworkQueue, Controller controller) {
+	public Thread1(FromRemoteInterface xbee) {
 		
-		//this.toXBeeNetworkQ = toXBeeNetworkQueue;
-		//this.fromXBeeNetworkQ = fromXBeeNetworkQueue;
-		//this.controller = controller;
+		this.toXBeeNetworkQ = xbee;
+		// TODO Add the WebSocket interface.
+	}
+	
+	public void updateState(RemoteData dataPacket)
+	{
+			
+	}
+	
+	public void sendToClients(RemoteData dataPaket)
+	{
+		
 	}
 
+	/**
+	 * Running body of this thread it will monitor the remote networks for info and 
+	 * update the master state list then send the update out to the websocket clients.
+	 */
 	@Override
-	public void run() {
-		while(true) {
-			// TODO WRITE THE TASK RUN BY THE THREAD (LOOP AS LONG AS THE PROGRAM HAS NOT EXITED)
-			System.out.println("Time: " + new java.util.Date() + ", THREAD 1");
-			try {
-				this.wait(10*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public void run() 
+	{
+		while(true) 
+		{
+			//If the queue has a message then get it
+			if(this.toXBeeNetworkQ.hasMessages())
+			{
+				//Remove the packet
+				RemoteData packet = this.toXBeeNetworkQ.getRemoteMessage();
+				System.out.println("Thread1: Received a message from a remote:" + packet.getAddress());
+				//update the state list and broadcast to all clients.
+				updateState(packet);
+				sendToClients(packet);				
+			}
+			//Else sleep for a bit
+			else			
+			{
+				try {
+					Thread.sleep(10*1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
--- ISISServer/src/remoteInterface/ToRemoteInterface.java
+++ ISISServer/src/remoteInterface/ToRemoteInterface.java
@@ -1,7 +1,19 @@
 package remoteInterface;
 
+/**
+ * This interface will be used to send commands to the 
+ * IO network. The command object will be passed to the 
+ * ToNetworkQueue. 
+ * 
+ * @author noquarter
+ *
+ */
 public interface ToRemoteInterface {
 	
+	/**
+	 * Send a RemoteCommand message to the external IO controller.
+	 * @param msg
+	 */
 	public void sendDataToRemote(RemoteCommand msg);
 
 }
